b555f9f9f8b0ee50d4242dd33fcef06e37b065d9
Ahmadm-dl <ahmad..m.mustapha@hotmail.com>
2024-09-19 18:58:00 +0300

    Predictions as probabilities

--- run.py
+++ run.py
@@ -1,15 +1,18 @@
 import argparse, os, json
-import transformers
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from utils import PromptFactory
+import torch
+import numpy as np
 
 CHECKPOINTS_DIR = "./checkpoints/"
 
 def setup():
-    os.environ["HF_TOKEN"] = "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"
-    if os.path.exists(CHECKPOINTS_DIR):
-        pass
+    if os.path.exists(".env"):
+        token = [l.split("=")[1] for l in open(".env", "r").readlines()][0]
+        os.environ["HF_TOKEN"] = token
     else:
+        raise Exception("Please add an .env file that includes huggingface HF_TOKEN")
+    if not os.path.exists(CHECKPOINTS_DIR):
         os.mkdir(CHECKPOINTS_DIR)
     
 
@@ -19,15 +22,9 @@ def load_data(data_path):
 
 def load_model(model_identifier):
     tokenizer = AutoTokenizer.from_pretrained(model_identifier)
-    generator = transformers.pipeline(
-    "text-generation",
-    model=model_identifier,
-    tokenizer=tokenizer,
-    #torch_dtype=torch.bfloat16,
-    trust_remote_code=True,
-    device_map="auto",
-    return_full_text=False)
-    return generator, tokenizer
+    model = AutoModelForCausalLM.from_pretrained(model_identifier, trust_remote_code=True, device_map="auto")
+    model.eval()
+    return model, tokenizer
 
 def load_checkpoint(model_identifier):
     filepath = os.path.join(CHECKPOINTS_DIR, f"{model_identifier}.output")
@@ -41,37 +38,57 @@ def save_checkpoint(model_identifier, output):
     filepath = os.path.join(CHECKPOINTS_DIR, f"{model_identifier}.output")
     json.dump(output, open(filepath, "w"), ensure_ascii=False, indent=4)
 
+def softmax(x):
+    z = x - max(x)
+    numerator = np.exp(z)
+    denominator = np.sum(numerator)
+    softmax = numerator/denominator
+    return softmax
+
 def main():
     parser = argparse.ArgumentParser(description="A script that loads a model and compute inference on a dataset.")
     parser.add_argument('-m', '--model', type=str, required=True, help="Huggingface model identifier")
     parser.add_argument('-d', '--data', type=str, required=True, help="MCQ data to infere on")
-    parser.add_argument('-s', '--maxsize', type=int, required=True, help="Generated text max. size")
-    # parser.add_argument('-x', '--xxx', type=str, required=True, help="xxx")
-    # parser.add_argument('-x', '--xxx', type=int, required=True, help="xxx")
-    # parser.add_argument('-x', '--xxx', action='store_true', help="xxx")
+    parser.add_argument('-s', '--max_input_token', type=int, required=True, help="Max input tokens a model can consume")
+    parser.add_argument('-v', '--verbose', action='store_true', help="Verbose")
     args = parser.parse_args()
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
     setup()
     
     data = load_data(args.data)
     
-    generator, tokenizer = load_model(args.model)
+    model, tokenizer = load_model(args.model)
     
     prompt_factory = PromptFactory()
     prompt_generator = prompt_factory.get_prompt_function(n_shots=0)
 
     output = load_checkpoint(args.model)
-    checkpoint = output[-1].id
+    checkpoint = output[-1]["id"]
 
     for i, q in enumerate(data[checkpoint+1:]):
         id = i + checkpoint + 1
         question = q["question"]
         options = [q[f"option_{i}"] for i in [0, 1, 2, 3] if f"option_{i}" in q and q[f"option_{i}"] != "" ]
         prompt = prompt_generator(question, options, subject=q["subject"], level=q["level"])
-        generated_outputs = generator(question, pad_token_id= tokenizer.eos_token_id, max_new_tokens= args.maxsize)
-        generated_output = generated_outputs[0]['generated_text']
-        output.append({"id": id, "generated_text": generated_output})
+        options_tokens = [tokenizer.encode(choice)[-1] for choice in ["0", "1", "2", "3", "4"]]
+        with torch.no_grad():
+            inputs = tokenizer(prompt, return_tensors="pt")
+            input_size = inputs['input_ids'].size(1)
+            if input_size > args.max_input_token:
+                raise ValueError(f"Input {id} is too long! The tokenized input has {input_size} tokens, which exceeds the maximum allowed size of {args.max_input_token} tokens.")
+            input_ids = inputs["input_ids"].to(device)
+            outputs = model(**inputs)#, labels=input_ids)
+            last_token_logits = outputs.logits[:, -1, :]
+            options_tokens_logits = last_token_logits[:, options_tokens].detach().cpu().numpy()
+            conf = softmax(options_tokens_logits[0])
+            pred = np.argmax(options_tokens_logits[0])
+        output.append({"id": id, "prediction": pred, "confidence": conf[pred]})
         save_checkpoint(args.model, output)
+        if id>0 and id%100==0:
+            if args.verbose: 
+                print(f"Processed {id} questions")
 
 if __name__ == '__main__':
     main()
