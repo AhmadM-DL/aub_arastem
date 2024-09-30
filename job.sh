8347a220e599af0d4f7e5096fd6c8d50075ef38d
Ahmadm-dl <ahmad..m.mustapha@hotmail.com>
2024-09-30 17:44:33 +0300

    Fix job

--- job.sh
+++ job.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=arastem_eval  
+#SBATCH --job-name=arastem_eval 
+
+#SBATCH --partition=gpu
 #SBATCH --nodes=1                      
 #SBATCH --ntasks=1                                
-#SBATCH --gres=gpu:1                   
-#SBATCH --mem=16G                      
+#SBATCH --gres=gpu:v100d32q:4                   
+
+#SBATCH --mem=32000                      
 #SBATCH --time=06:00:00                
 #SBATCH --output=output_%j.log         
 #SBATCH --error=error_%j.log
@@ -12,7 +15,7 @@ module load python/3
 source /home/am252/scratch/myenv/bin/activate
 
 MODEL = "inceptionai/jais-30b-v1"
-DATA = "/home/am252/data/arastem.json"
+DATA = "/home/am252/scratch/arastem/data/arastem.json"
 MAX_INPUT_TOKENS = 2048
 python run.py --model $MODEL --data $DATA -s $MAX_INPUT_TOKENS -v
 
