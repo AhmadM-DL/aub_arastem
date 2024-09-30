20c4ea5648d6682cd5b82deac9bd27f874ed7070
Ahmadm-dl <ahmad..m.mustapha@hotmail.com>
2024-09-30 17:37:43 +0300

    Add job

---
+++ job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=arastem_eval  
+#SBATCH --nodes=1                      
+#SBATCH --ntasks=1                                
+#SBATCH --gres=gpu:1                   
+#SBATCH --mem=16G                      
+#SBATCH --time=06:00:00                
+#SBATCH --output=output_%j.log         
+#SBATCH --error=error_%j.log
+
+module load python/3
+source /home/am252/scratch/myenv/bin/activate
+
+MODEL = "inceptionai/jais-30b-v1"
+DATA = "/home/am252/data/arastem.json"
+MAX_INPUT_TOKENS = 2048
+python run.py --model $MODEL --data $DATA -s $MAX_INPUT_TOKENS -v
+
