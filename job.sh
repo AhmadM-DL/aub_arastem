a519404adc753ec4d0dc3889e8e5d25fec04b431
Ahmadm-dl <ahmad..m.mustapha@hotmail.com>
2024-10-01 14:01:22 +0300

    Fix job

--- job.sh
+++ job.sh
@@ -13,8 +13,9 @@
 module load python/3
 source /home/am252/scratch/myenv/bin/activate
 
-MODEL="inceptionai/jais-30b-v1"
+MODEL=""
 DATA="/home/am252/scratch/aub_arastem/data/arastem.json"
 MAX_INPUT_TOKENS=2048
-python run.py --model $MODEL --data $DATA -s $MAX_INPUT_TOKENS -v
+HF_CACHE="/home/am252/scratch/hf_models/"
+python run.py --model $MODEL --data $DATA -s $MAX_INPUT_TOKENS -hf $HF_CACHE -v
 
