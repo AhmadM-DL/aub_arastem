b92baa287c91e5c240aad13a170bfdad1579fe89
Ahmadm-dl <ahmad..m.mustapha@hotmail.com>
2024-10-22 14:47:31 +0300

    Hide Token

--- run_hf.py
+++ run_hf.py
@@ -29,11 +29,11 @@ def save_checkpoint(destination, model_identifier, output):
     filepath = os.path.join(destination, CHECKPOINTS_DIR, f"{model_identifier}.output")
     json.dump(output, open(filepath, "w"), ensure_ascii=False, indent=4)
 
-def infere_from_model(endpoint, prompt):
+def infere_from_model(endpoint, prompt, token):
     payload = {"inputs":"", "prompt": prompt}
     headers = {
         "Accept" : "application/json",
-        "Authorization": "Bearer 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",
+        "Authorization": f"Bearer {token}",
         "Content-Type": "application/json" 
     }
     response = requests.post(endpoint, headers=headers, json=payload)
@@ -47,6 +47,7 @@ def main():
     parser = argparse.ArgumentParser(description="A script that requests a model and compute inference on a dataset.")
     parser.add_argument('-m', '--model', type=str, required=True, help="Model Identifier")
     parser.add_argument('-e', '--endpoint', type=str, required=True, help="Model Endpoint")
+    parser.add_argument('-t', '--token', type=str, required=True, help="HF Token")
     parser.add_argument('-d', '--data', type=str, required=True, help="MCQ data to infere on")
     parser.add_argument('-r', '--root', type=str, default=".", help="The ouput destination")
     parser.add_argument('-v', '--verbose', action='store_true', help="Verbose")
@@ -72,7 +73,7 @@ def main():
         question = q["question"]
         options = [q[f"option_{i}"] for i in [0, 1, 2, 3] if f"option_{i}" in q and q[f"option_{i}"] != "" ]
         prompt = prompt_generator(question, options, subject=q["subject"], level=q["level"])
-        pred, conf = infere_from_model(args.endpoint, prompt)
+        pred, conf = infere_from_model(args.endpoint, prompt, args.token)
         output.append({"id": id, "prediction": int(pred), "confidence": float(conf[pred])})
         save_checkpoint(args.root, args.model, output)
         if id>0 and id%100==0:
